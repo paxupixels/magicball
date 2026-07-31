f7a154efcd7002537a047613b9e9463c8d75dda9
Noah Neuhaus <noah.neuhaus@ptb.de>
2026-07-31 10:08:59 +0200

    Fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,18 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
+
 const config = {
+	compilerOptions: {
+		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+	},
 	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/magicball' : ''
 		}
