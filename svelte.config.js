86fb34f868382660c1a893848741497355b84e55
Noah Neuhaus <noah.neuhaus@ptb.de>
2026-07-31 09:15:26 +0200

    GitHub-Push vorbereitet

---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/magicball' : ''
+		}
+	}
+};
+
+export default config;
